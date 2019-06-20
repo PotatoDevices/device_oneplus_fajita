73d844bdca162900ced34808583dd0da81669052
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-06-20 17:52:14 +0530

    fajita: overlay: Remove NoCutoutOverlay and commonise it
    See: https://review.aosiprom.com/c/AOSIP-Devices/device_oneplus_sdm845-common/+/8234
    Change-Id: I643676b32c1c5d7132d16820142673670fb5546c
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- device.mk
+++ device.mk
@@ -27,9 +27,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay
-
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
