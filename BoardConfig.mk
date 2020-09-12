adb08826edc7ea27a1e2a34b21ad43eb099e1ea1
Ziauddin Sameer <ziauddinsameer@gmail.com>
2020-09-13 02:11:11 +0600

    fajita: Update Prebuilt TWRP recovery method

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,8 +35,8 @@ DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_fajita
 TARGET_RECOVERY_DEVICE_MODULES := libinit_fajita
 
-# Prebuilt TWRP BLACK recovery
-TARGET_PREBUILT_RECOVERY_RAMDISK_CPIO := device/oneplus/fajita/twrp/ramdisk-recovery.cpio
+# Prebuilt TWRP recovery
+TARGET_PREBUILT_RECOVERY_RAMDISK := $(DEVICE_PATH)/ramdisk-recovery.zip
 
 # inherit from the proprietary version
 -include vendor/oneplus/fajita/BoardConfigVendor.mk
